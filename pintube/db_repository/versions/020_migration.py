6f1fab77cdbe8fa271cc1802e69fb1feb8a2e0fb
Mark Folashade <ayodeji.folashade@gmail.com>
2014-02-19 06:39:23 -0600

    Some UI changes

---
+++ pintube/db_repository/versions/020_migration.py
@@ -0,0 +1,35 @@
+from sqlalchemy import *
+from migrate import *
+
+
+from migrate.changeset import schema
+pre_meta = MetaData()
+post_meta = MetaData()
+users = Table('users', pre_meta,
+    Column('id', Integer, primary_key=True, nullable=False),
+    Column('username', String(length=None, convert_unicode=False, assert_unicode=None, unicode_error=None, _warn_on_bytestring=False)),
+    Column('last_updated', String(length=None, convert_unicode=False, assert_unicode=None, unicode_error=None, _warn_on_bytestring=False)),
+)
+
+user = Table('user', post_meta,
+    Column('id', Integer, primary_key=True, nullable=False),
+    Column('username', String(length=80, convert_unicode=False, assert_unicode=None, unicode_error=None, _warn_on_bytestring=False)),
+    Column('last_updated', String(length=120, convert_unicode=False, assert_unicode=None, unicode_error=None, _warn_on_bytestring=False)),
+)
+
+
+def upgrade(migrate_engine):
+    # Upgrade operations go here. Don't create your own engine; bind
+    # migrate_engine to your metadata
+    pre_meta.bind = migrate_engine
+    post_meta.bind = migrate_engine
+    pre_meta.tables['users'].drop()
+    post_meta.tables['user'].create()
+
+
+def downgrade(migrate_engine):
+    # Operations to reverse the above upgrade go here.
+    pre_meta.bind = migrate_engine
+    post_meta.bind = migrate_engine
+    pre_meta.tables['users'].create()
+    post_meta.tables['user'].drop()
