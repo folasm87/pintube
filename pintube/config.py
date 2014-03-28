07d863b06dd78364fc32ada3e9e69a3beb3a458b
Mark Folashade <ayodeji.folashade@gmail.com>
2014-03-27 20:06:11 -0500

    Added Database Configuration

--- pintube/config.py
+++ pintube/config.py
@@ -3,7 +3,8 @@ import psycopg2
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-SQLALCHEMY_DATABASE_URI = "postgresql://folasm87:GoatKiller87@localhost/pintube_app"
+# SQLALCHEMY_DATABASE_URI = "postgresql://folasm87:GoatKiller87@localhost/pintube_app"
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SQLALCHEMY_ECHO = True
 CSRF_ENABLED = True
